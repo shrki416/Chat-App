184ca7ad7e411aa3bfe0ae8f8b82461b2e8f6a01
shrki416 <coach-ahmed@bmsnj.org>
2020-03-15 21:10:10 -0400

    Add pg dependency for db

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return <h1>Chat App</h1>;
 };
 
 export default App;
--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "client/public")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
