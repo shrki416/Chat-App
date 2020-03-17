40a7beb3688040def3224c253c814cae4c362855
shrki416 <coach-ahmed@bmsnj.org>
2020-03-17 16:44:22 -0400

    Header component up to date

---
+++ Client/src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import chat from "./assets/chat_icon.png";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <img src={chat} />
+        <h1>Chat App</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
