649c42491aa845499722e54b1f25120ee57cd876
shrki416 <coach-ahmed@bmsnj.org>
2020-03-25 11:51:53 -0400

    Use dummy data for messages and users to work with

---
+++ Client/src/components/data.js
@@ -0,0 +1,17 @@
+const userAhmed = {
+  name: "Ahmed",
+  messages: [
+    { text: "Hey there!" },
+    { text: "How is everyone doing??" },
+    { text: "So glad this chat is up and running!" }
+  ]
+};
+
+const userSelf = {
+  name: "You",
+  messages: [{ text: "I know me too!" }, { text: "This is so much fun" }]
+};
+
+const users = [userAhmed, userSelf];
+
+export default users;
