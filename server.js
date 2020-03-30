ff586def9904cb94de4877bcc59053501fe32c26
shrki416 <coach-ahmed@bmsnj.org>
2020-03-30 15:01:59 -0400

    Connect to db using node

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const path = require("path");
-// const db = require("./queries");
+const bodyParser = require("body-parser");
+const { Pool, Client } = require("pg");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const pool = new Pool({
+  user: "AA",
+  host: "localhost",
+  database: "chat_app",
+  password: "chat",
+  port: 5432
+});
+
 app.use(express.static(path.join(__dirname, "client/build")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/messages", (request, response) => {
+  pool.query("SELECT * FROM messages", (err, res) => {
+    if (err) {
+      console.log(`Something went wrong: Error is ${err}`);
+    } else {
+      console.log(res.rows);
+    }
+  });
+});
+
+app.get("/api/users", (request, response) => {
+  pool.query("SELECT * FROM users", (err, res) => {
+    if (err) {
+      console.log(`Something went wrong: Error is ${err}`);
+    } else {
+      console.log(res.rows);
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
