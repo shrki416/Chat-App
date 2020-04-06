12ac7564def413f760a3f93556b0fe8366308de0
shrki416 <coach-ahmed@bmsnj.org>
2020-04-06 19:55:09 -0400

    Able to display messages from db into chat box

--- Client/src/components/Chat.js
+++ Client/src/components/Chat.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import send from "../assets/send-icon.png";
 import add from "../assets/add-icon.png";
-import data from "./data";
 
 const Chat = () => {
-  const addMessage = e => {
+  const [messages, setMessages] = useState({});
+
+  const getMessages = async () => {
+    try {
+      const db_messages = await fetch("/messages");
+      const messages = await db_messages.json();
+      setMessages(messages);
+      console.log(messages);
+    } catch (error) {
+      console.log(`Something went wrong: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const messagesArr = Object.values(messages);
+
+  const displayMessages = messagesArr.map((message) => {
+    return (
+      <p key={message.id}>
+        {message.userid}: {message.text}
+      </p>
+    );
+  });
+
+  const addMessage = (e) => {
     console.log("message sent");
   };
 
-  const addUser = e => {
+  const addUser = (e) => {
     e.preventDefault();
-    
+
     console.log("user added");
   };
 
@@ -19,7 +45,7 @@ const Chat = () => {
       <div className="user-container">
         <div className="users">
           <h2>Users</h2>
-          <p>{data[0].name}</p>
+          <p>Ahmed</p>
         </div>
         <div>
           <button className="add-user" onClick={addUser}>
@@ -30,15 +56,7 @@ const Chat = () => {
       <div className="message-container">
         <div className="messages">
           <h2>Messages</h2>
-          <p>
-            {data[0].name}: {data[0].text}
-          </p>
-          <p>
-            {data[1].name}: {data[1].text}
-          </p>
-          <p>
-            {data[2].name}: {data[2].text}
-          </p>
+          {displayMessages}
         </div>
         <div className="add-message">
           <input placeholder="what's on your mind?" />
--- Client/src/components/data.js
+++ Client/src/components/data.js
@@ -1,7 +1,7 @@
 const data = [
   { id: "1", name: "Ahmed", text: "Hi there!" },
   { id: "2", name: "Ahmed", text: "Hi, how are you?" },
-  { id: "3", name: "Ahmed", text: "good!" }
+  { id: "3", name: "Ahmed", text: "good!" },
 ];
 
 export default data;
