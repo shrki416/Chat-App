282a0ea65579a0acad8f8d59e652b03fc773776f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-05 19:30:20 -0500

    Creat Auth folder which will contain a login and a register component

---
+++ Client/src/components/Auth/Register.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import registerImage from "../../assets/register.svg";
+import "./Auth.css";
+
+function Register() {
+  return (
+    <div id="auth-container">
+      <div id="auth-card">
+        <div className="card-shadow">
+          <div id="image-section">
+            <img src={registerImage} alt="Register" />
+          </div>
+
+          <div id="form-section">
+            <h2>Create an account</h2>
+
+            {/* <form onSubmit={submitForm}> */}
+            <form>
+              <div className="input-field mb-1">
+                <input
+                  //   onChange={(e) => setFirstName(e.target.value)}
+                  //   value={firstName}
+                  //   required="required"
+                  type="text"
+                  placeholder="First name"
+                />
+              </div>
+
+              <div className="input-field mb-1">
+                <input
+                  //   onChange={(e) => setLastName(e.target.value)}
+                  //   value={lastName}
+                  //   required="required"
+                  type="text"
+                  placeholder="Last name"
+                />
+              </div>
+
+              <div className="input-field mb-1">
+                <input
+                  //   onChange={(e) => setEmail(e.target.value)}
+                  //   value={email}
+                  //   required="required"
+                  type="text"
+                  placeholder="Email"
+                />
+              </div>
+
+              <div className="input-field mb-1">
+                <select
+                //   onChange={(e) => setGender(e.target.value)}
+                //   value={gender}
+                //   required="required"
+                >
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+
+              <div className="input-field mb-2">
+                <input
+                  //   onChange={(e) => setPassword(e.target.value)}
+                  //   value={password}
+                  //   required="required"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+
+              <button>REGISTER</button>
+            </form>
+
+            <p>
+              Already have an account? <Link to="/login">LOGIN</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
