7bf4736b8dd639e2095e9cc11dba6a0733e0be5f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-07 18:26:56 -0500

    WIP: Auth

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const pool = require("./db");
+const router = require("./router");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,27 +10,8 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/messages", async (req, res) => {
-  try {
-    const messages = await pool.query("SELECT * FROM messages");
-    console.table(messages.rows);
-    res.json(messages.rows);
-  } catch (error) {
-    console.log(`Something went wrong: ${error}`);
-    res.sendStatus(500);
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await pool.query("SELECT * FROM users");
-    console.table(users.rows);
-    res.json(users.rows);
-  } catch (error) {
-    console.log(`Something went wrong: ${error}`);
-    res.sendStatus(500);
-  }
-});
+// login route
+app.post("/login", (req, res) => {});
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
