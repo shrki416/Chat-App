276330a7a422cf95c05a154ea34c3c5f6db152a4
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-09 16:07:50 -0500

    Add database folder

---
+++ database/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+module.exports = pool;
