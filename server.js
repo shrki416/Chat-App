ee9abb383d135456712edafdd2e5d988a04ebfb7
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-13 20:36:01 -0500

    User Auth: WIP

--- server.js
+++ server.js
@@ -2,21 +2,33 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const pool = require("./database/db");
+const { response } = require("express");
 // const router = require("./router");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // login route
-app.post("/login", (req, res) => {});
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+  console.log({ email, password });
 
-app.post("/register", (req, res) => {});
+  res.status(201).send();
+});
+
+app.post("/api/register", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  console.log({ firstName }, { lastName }, { email }, { password });
+
+  res.status(201).send();
+});
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}!`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
