30af3a3e121835fae36307ea4097177a8b64bf2d
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-18 21:47:19 -0400

    registration route: WIP

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const pool = require("./database/db");
-const { response } = require("express");
+const validate = require("./middleware/validate");
+
 // const router = require("./router");
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// login route
+// Login Route
 app.post("/api/login", (req, res) => {
   const { email, password } = req.body;
   console.log({ email, password });
@@ -20,11 +21,20 @@ app.post("/api/login", (req, res) => {
   res.status(201).send();
 });
 
-app.post("/api/register", async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-  console.log({ firstName }, { lastName }, { email }, { password });
+// Register Route
+app.post("/register", validate, async (req, res) => {
+  // 1. destructure req.body (name, email, password)
+  // 2. check if user exist (if user exist then throw error)
+  // 3. bcrypt the user password
+  // 4. enter the new user inside our database
+  // 5. generating out jwt token
 
-  res.status(201).send();
+  try {
+    const { firstName, lastName, email, password } = req.body;
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 app.get("/*", (req, res) => {
