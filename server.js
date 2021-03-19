20a72983d941bd6eb037bb8a2ff0324eb40afe1f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-19 14:56:05 -0400

    Complete postman testing of register route

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const pool = require("./database/db");
-const validate = require("./middleware/validate");
-
-// const router = require("./router");
+const register = require("./router/register");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,20 +19,7 @@ app.post("/api/login", (req, res) => {
 });
 
 // Register Route
-app.post("/register", validate, async (req, res) => {
-  // 1. destructure req.body (name, email, password)
-  // 2. check if user exist (if user exist then throw error)
-  // 3. bcrypt the user password
-  // 4. enter the new user inside our database
-  // 5. generating out jwt token
-
-  try {
-    const { firstName, lastName, email, password } = req.body;
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
-  }
-});
+app.use("/", register);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
