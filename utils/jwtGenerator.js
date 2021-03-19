d76b4a1510fafc34e16eece4601b628c160a347b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-18 21:46:47 -0400

    add utils directory

---
+++ utils/jwtGenerator.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const jwtGenerator = (id) => {
+  const payload = {
+    user: id,
+  };
+  return jwt.sign(payload, process.env.SECRET, { expiresIn: 3600 });
+};
+
+module.exports = jwtGenerator;
