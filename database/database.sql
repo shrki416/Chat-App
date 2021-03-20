a3b2b8a183eda26e2e8147c82cc433945ac415bd
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-20 13:15:19 -0400

    Rename columns

--- database/database.sql
+++ database/database.sql
@@ -9,4 +9,7 @@ CREATE TABLE users(
     password VARCHAR(255) NOT NULL
 );
 
+ALTER TABLE users RENAME COLUMN first_name TO firstName;
+ALTER TABLE users RENAME COLUMN last_name TO lastName;
+
 INSERT INTO users (first_name, last_name, email, password) VALUES ('John', 'Smith', 'john@gmail.com', 'password');
