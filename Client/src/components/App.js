f7a541241818a30b09389e81d1a95c4c7cceebf0
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-20 13:16:30 -0400

    Logining in a user: WIP

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "../components/Auth/Login";
 import Register from "../components/Auth/Register";
 import Chat from "../components/Chat";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "../App.css";
+import axios from "axios";
 
 const App = () => {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
+
+  const authenticateUser = (Boolean) => setIsUserAuthenticated(Boolean);
+
+  const userAuth = async () => {
+    try {
+      const response = await axios.get("/api/verify");
+      response ? setIsUserAuthenticated(true) : setIsUserAuthenticated(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => authenticateUser(), []);
+
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Chat} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
+          <Route exact path="/" component={Chat} auth={authenticateUser} />
+          {!isUserAuthenticated ? (
+            <Route path="/login" component={Login} auth={authenticateUser} />
+          ) : (
+            <Route
+              path="/register"
+              component={Register}
+              auth={authenticateUser}
+            />
+          )}
           <Route render={() => <h1>404 page not found</h1>} />
         </Switch>
       </div>
