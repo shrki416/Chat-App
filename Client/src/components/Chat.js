cbe086050777135154a9c68a76f304b99d04ee3f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-21 18:04:44 -0400

    Finally, able to get usern first and last name and display in a welcome message

--- Client/src/components/Chat.js
+++ Client/src/components/Chat.js
@@ -1,12 +1,37 @@
-import React from "react";
-import Messages from "./Messages";
-import Users from "./Users";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import Messages from "./Messages";
+// import Users from "./Users";
 
-const Chat = () => {
+const Chat = ({ auth }) => {
+  const [name, setName] = useState("");
+
+  const getUserProfile = async () => {
+    try {
+      const config = {
+        headers: {
+          token: localStorage.token,
+        },
+      };
+
+      await axios.get("/api/user", config).then((res) => setName(res.data));
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const { firstname, lastname } = name;
+
+  useEffect(() => getUserProfile(), []);
+
   return (
     <div>
-      <Messages />
-      <Users />
+      <h1>Chat</h1>
+      <h4>
+        Welcome {lastname}, {firstname}
+      </h4>
+      {/* <Messages />
+      <Users /> */}
     </div>
   );
 };
