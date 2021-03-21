18cce48e87ea0b0d6a6cce2ceefc4f60ee162548
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-21 18:01:46 -0400

    Add user api route

---
+++ router/user.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const pool = require("../database/db");
+const auth = require("../middleware/auth");
+
+router.get("/user", auth, async (req, res) => {
+  try {
+    const user = await pool.query(
+      "SELECT firstName, lastName from users WHERE id=$1",
+      [req.user]
+    );
+    res.json(user.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Server Error");
+  }
+});
+
+module.exports = router;
