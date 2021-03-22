f392fa9a12b5d5923bd8bc07c1598a200ed89b67
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-22 14:07:18 -0400

    Getting the proper routing done

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -2,34 +2,71 @@ import React, { useEffect, useState } from "react";
 import Login from "../components/Auth/Login";
 import Register from "../components/Auth/Register";
 import Chat from "../components/Chat";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import "../App.css";
 import axios from "axios";
 
 const App = () => {
   const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
 
-  // const authenticateUser = (Boolean) => setIsUserAuthenticated(Boolean);
+  const authenticateUser = (Boolean) => setIsUserAuthenticated(Boolean);
 
-  // const userAuth = async () => {
-  //   try {
-  //     const response = await axios.get("/api/verify");
-  //     response ? setIsUserAuthenticated(true) : setIsUserAuthenticated(false);
-  //   } catch (error) {
-  //     console.error(error.message);
-  //   }
-  // };
+  const userAuth = async () => {
+    try {
+      const config = {
+        headers: { token: localStorage.token },
+      };
+      const response = await axios.get("/api/verify", config);
+      response ? setIsUserAuthenticated(true) : setIsUserAuthenticated(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-  // useEffect(() => authenticateUser(), []);
+  useEffect(() => userAuth(), []);
 
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Chat} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
+          <Route
+            exact
+            path="/"
+            render={(props) =>
+              isUserAuthenticated ? (
+                <Chat {...props} auth={authenticateUser} />
+              ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/login"
+            render={(props) =>
+              !isUserAuthenticated ? (
+                <Login {...props} auth={authenticateUser} />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/register"
+            render={(props) =>
+              !isUserAuthenticated ? (
+                <Register {...props} auth={authenticateUser} />
+              ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
           <Route render={() => <h1>404 page not found</h1>} />
         </Switch>
       </div>
