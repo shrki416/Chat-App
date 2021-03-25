317e9fe00a20a86c33ed5c85fad45862ff586d9a
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-25 17:14:45 -0400

    Begin working on chat-box UI

---
+++ Client/src/components/Chat/Chat.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+const Chat = ({ auth }) => {
+  return (
+    <>
+      <Navbar auth={auth} />
+
+      {/* <Users /> */}
+      {/* <Messages /> */}
+    </>
+  );
+};
+
+export default Chat;
