11eab0ab6d9cf812dbf0dfbe468d201339aa6a90
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-04-21 20:02:03 -0400

    Chat UI WIP

--- Client/src/components/Chat/Chat.js
+++ Client/src/components/Chat/Chat.js
@@ -1,143 +1,87 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import Navbar from "../Navbar/Navbar";
-import socketIOClient from "socket.io-client";
-import axios from "axios";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import { Box, makeStyles } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+// import IconButton from "@material-ui/core/IconButton";
 import "./Chat.css";
 
-const ENDPOINT = "http://localhost:3000";
+const defaultProps = {
+  bgcolor: "background.paper",
+  border: 1,
+  borderColor: "text.primary",
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      // margin: theme.spacing(1),
+      width: "100%",
+    },
+  },
+  center: {
+    padding: theme.spacing(3, 2),
+    height: "70vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  container: {
+    display: "grid",
+    gridTemplateColumns: "repeat(12, 1fr)",
+    // gridGap: theme.spacing(3),
+  },
+  paper: {
+    padding: theme.spacing(1),
+    height: "500px",
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    whiteSpace: "nowrap",
+    // marginBottom: theme.spacing(1),
+  },
+  divider: {
+    margin: theme.spacing(2, 0),
+  },
+}));
 
 const Chat = ({ auth }) => {
-  const [message, setMessage] = useState("");
-  const [text, setText] = useState("");
-  const [contact, setContact] = useState("");
-  const socket_ptr = useRef(null);
-  const login_ptr = useRef({});
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("message", (data) => {
-      console.log("Server Message", data);
-      setMessage(data);
-      try {
-        const config = {
-          headers: {
-            token: localStorage.token,
-          },
-        };
-
-        console.log("Get User Information");
-        axios.get("/api/user", config).then((res) => {
-          console.log(res.data);
-          login_ptr.current = res.data;
-          socket.emit("login", res.data);
-        });
-      } catch (error) {
-        console.error(error.message);
-      }
-    });
-
-    socket.on("room", (data) => console.log("Room Message", data));
-
-    socket.on("server", (data) =>
-      setMessage(`${data.message}, From : ${data.from}`)
-    );
-
-    socket_ptr.current = socket;
-  }, []);
-
-  const onSendMessage = () => {
-    var data = {
-      to: contact,
-      from: login_ptr.current.email,
-      message: text,
-    };
-
-    console.log("onSend Message", data);
-    socket_ptr.current.emit("message", data);
-  };
+  const classes = useStyles();
   return (
     <>
       <Navbar auth={auth} />
-
-      <section className="msger">
-        <header className="msger-header">
-          <div className="msger-header-title">
-            <i className="fas fa-comment-alt"></i> Chat
+      <Paper elevation={0} className={classes.center}>
+        <div className={classes.container}>
+          <div style={{ gridColumnEnd: "span 2" }}>
+            {/* <Paper className={classes.paper}>USERS</Paper> */}
           </div>
-        </header>
-
-        <main className="msger-chat">
-          <div className="msg left-msg">
-            <div className="msg-bubble">
-              <div className="msg-info">
-                <div className="msg-info-name">Arya Stark</div>
-                <div className="msg-info-time">12:45</div>
-              </div>
-
-              <div className="msg-text">
-                Hi, when are you coming to King's Landing?
-              </div>
-            </div>
+          <div style={{ gridColumnEnd: "span 3" }}>
+            <Box {...defaultProps} borderRight={0}>
+              <Paper elevation={0} className={classes.paper}>
+                USERS
+              </Paper>
+            </Box>
           </div>
-
-          <div className="msg right-msg">
-            <div className="msg-bubble">
-              <div className="msg-info">
-                <div className="msg-info-name">John Snow</div>
-                <div className="msg-info-time">12:46</div>
-              </div>
-
-              <div className="msg-text">Never</div>
-            </div>
+          <div style={{ gridColumnEnd: "span 5" }}>
+            <Box {...defaultProps} borderLeft={0}>
+              <Paper elevation={2} className={classes.paper} border={1}>
+                MESSAGES
+              </Paper>
+            </Box>
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField
+                id="filled-basic"
+                variant="filled"
+                label="Type your message here"
+                InputProps={{ endAdornment: <SendIcon /> }}
+              />
+            </form>
           </div>
-        </main>
-
-        <form className="msger-inputarea">
-          <input
-            type="text"
-            className="msger-input"
-            placeholder="Enter your message..."
-          />
-          <button
-            className="msger-send-btn"
-            onClick={() => {
-              onSendMessage();
-            }}
-          >
-            Send
-          </button>
-        </form>
-      </section>
-
-      {/* <p>It is message from Server {message}</p>
-
-      <div classNameName="input-field">
-        <input
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-          required="required"
-          type="text"
-          placeholder="Input Message"
-        />
-      </div>
-
-      <div classNameName="input-field">
-        <input
-          onChange={(e) => setContact(e.target.value)}
-          value={contact}
-          required="required"
-          type="text"
-          placeholder="To"
-        />
-      </div>
-
-      <button
-        onClick={() => {
-          onSendMessage();
-        }}
-      >
-        Echo Send
-      </button> */}
+          <div style={{ gridColumnEnd: "span 2" }}>
+            {/* <Paper className={classes.paper}>USERS</Paper> */}
+          </div>
+        </div>
+      </Paper>
     </>
   );
 };
