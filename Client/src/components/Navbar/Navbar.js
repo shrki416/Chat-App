42d79d3b6e205b270f2cde2ea17cfaa25f9f4092
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-07 19:22:46 -0400

    Move user profile to main chat window instead of navbar

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -1,41 +1,16 @@
-// import React, { useState, useEffect } from "react";
 import React from "react";
-// import axios from "axios";
 import "./Navbar.css";
 
 function Navbar({ auth }) {
-  // const [name, setName] = useState("");
-
-  // const getUserProfile = async () => {
-  //   try {
-  //     const config = {
-  //       headers: {
-  //         token: localStorage.token,
-  //       },
-  //     };
-
-  //     await axios.get("/api/user", config).then((res) => setName(res.data));
-  //   } catch (error) {
-  //     console.error(error.message);
-  //   }
-  // };
-
-  // const { firstname, lastname } = name;
-
   const logout = (e) => {
     e.preventDefault();
     localStorage.removeItem("token");
     auth(false);
   };
 
-  // useEffect(() => getUserProfile(), []);
-
   return (
     <div id="navbar" className="card-shadow">
       <h2>Chat-App</h2>
-      {/* <p>
-        Welcome, {firstname} {lastname}!
-      </p> */}
       <button onClick={(e) => logout(e)}>LOGOUT</button>
     </div>
   );
