d9e0e40b7120d6d1a2f858a3dc271bdf20b79557
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-09 20:40:48 -0400

    WIP: diplaying messages properly

--- Client/src/components/Chat/Chat Message/ChatMessage.js
+++ Client/src/components/Chat/Chat Message/ChatMessage.js
@@ -2,14 +2,30 @@ import React from "react";
 
 import "./ChatMesasge.css";
 
-function ChatMessage({ message }) {
-  const chatBubbles = message.id % 2 === 0 ? "you-message" : "other-message";
+function ChatMessage({ message, user }) {
+  const id = message.user_id;
+  const chatBubbles = user.id === id ? "you-message" : "other-message";
+
+  const { firstname, lastname } = user;
+  const userBubble = user.id === id && `${firstname} ${lastname}`;
+
+  const formatDate = (date) => {
+    const messageDate = new Date(date);
+    const options = {
+      month: "short",
+      day: "numeric",
+    };
+
+    return messageDate.toLocaleDateString("en-US", options);
+  };
 
   return (
     <div className={`message-row ${chatBubbles}`}>
       <div className="message-content">
         <div className="message-text card-shadow">{message.message}</div>
-        <div className="message-time">{message.time}</div>
+        <div className="message-date">
+          {userBubble} {formatDate(message.created_at)}
+        </div>
       </div>
     </div>
   );
