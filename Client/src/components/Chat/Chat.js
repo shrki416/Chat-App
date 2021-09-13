b1937b5966219cd27cd220e59c7bc04a2a10614a
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-12 20:38:52 -0400

    Refactor

--- Client/src/components/Chat/Chat.js
+++ Client/src/components/Chat/Chat.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import DeleteIcon from "@material-ui/icons/Delete";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 import { io } from "socket.io-client";
 import axios from "axios";
@@ -11,6 +10,7 @@ import axios from "axios";
 import "./Chat.css";
 import ChatForm from "./Chat Form/ChatForm";
 import ChatMessages from "./Chat Messages/ChatMessages";
+import ConversationList from "./ConversationList";
 
 const Chat = ({ auth }) => {
   const [messages, setMessages] = useState([]);
@@ -78,72 +78,7 @@ const Chat = ({ auth }) => {
           <div id="search-container">
             <input type="text" name="search-message-box" placeholder="Search" />
           </div>
-          <div id="conversation-list">
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">Jon Snow</div>
-              <div className="created-date">Apr 16</div>
-              <div className="conversation-message">This is a message</div>
-            </div>
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">Kim O'Neil</div>
-              <div className="created-date">2 days ago</div>
-              <div className="conversation-message">Very funny</div>
-            </div>
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">John Anderson</div>
-              <div className="created-date">1 week ago</div>
-              <div className="conversation-message">
-                Yes I love how Python does that
-              </div>
-            </div>
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">Ben Smith</div>
-              <div className="created-date">2:49 PM</div>
-              <div className="conversation-message">
-                Yeah Miami Heat are done
-              </div>
-            </div>
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">Douglas Johannasen</div>
-              <div className="created-date">6:14 PM</div>
-              <div className="conversation-message">No it does not</div>
-            </div>
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">Jacob Manly</div>
-              <div className="created-date">3 secs ago</div>
-              <div className="conversation-message">
-                Just be very careful doing that
-              </div>
-            </div>
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">Stacey Wilson</div>
-              <div className="created-date">30 mins ago</div>
-              <div className="conversation-message">
-                Awesome!!! Congratulations!!!!
-              </div>
-            </div>
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">Stan George</div>
-              <div className="created-date">1 week ago</div>
-              <div className="conversation-message">Good job</div>
-            </div>
-            <div className="conversation">
-              <AccountCircleIcon fontSize="large" />
-              <div className="title-text">Sarah Momes</div>
-              <div className="created-date">1 year ago</div>
-              <div className="conversation-message">
-                Thank you. I appreciate that.
-              </div>
-            </div>
-          </div>
+          <ConversationList />
           <div id="new-message-container">
             <AddCircleIcon fontSize="large" id="add-icon" />
           </div>
