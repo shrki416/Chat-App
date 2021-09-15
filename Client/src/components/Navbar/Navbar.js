12873f24c4c84038a71032db64f3cb870a259a51
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-14 21:40:40 -0400

    Move user info back to Nav

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Navbar.css";
 
-function Navbar({ auth }) {
+function Navbar({ auth, user }) {
   const logout = (e) => {
     e.preventDefault();
     localStorage.removeItem("token");
     auth(false);
   };
 
+  const { firstname, lastname } = user;
+
   return (
     <div id="navbar" className="card-shadow">
       <h2>Chat-App</h2>
+      <p>{user && `Welcome, ${firstname} ${lastname}!`}</p>
       <button onClick={(e) => logout(e)}>LOGOUT</button>
     </div>
   );
