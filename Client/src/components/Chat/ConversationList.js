587d333e37daf85a3969d8e227c000b9f322ea31
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-21 11:58:52 -0400

    Clean up

--- Client/src/components/Chat/ConversationList.js
+++ Client/src/components/Chat/ConversationList.js
@@ -5,54 +5,6 @@ import "./Chat.css";
 import axios from "axios";
 import * as timeago from "timeago.js";
 
-// const users = [
-//   {
-//     name: "John Anderson",
-//     created: "1 week ago",
-//     message: "Yes I love how Python does that",
-//   },
-//   {
-//     name: "Ben Smith",
-//     created: "2:49 PM",
-//     message: "Yeah Miami Heat are done",
-//   },
-//   {
-//     name: "Douglas Johannasen",
-//     created: "6:14 PM",
-//     message: "No it does not",
-//   },
-//   {
-//     name: "Jacob Manly",
-//     created: "3 secs ago",
-//     message: "Just be very careful doing that",
-//   },
-//   {
-//     name: "Stacey Wilson",
-//     created: "30 mins ago",
-//     message: "Awesome!!! Congratulations!!!!",
-//   },
-//   {
-//     name: "Stan George",
-//     created: "1 week ago",
-//     message: "Good job",
-//   },
-//   {
-//     name: "Sarah Momes",
-//     created: "1 year ago",
-//     message: "Thank you. I appreciate that.",
-//   },
-//   {
-//     name: "Kim O'Neil",
-//     created: "2 days ago",
-//     message: "Very funny",
-//   },
-//   {
-//     name: "Jon Snow",
-//     created: "Apr 16",
-//     message: "This is a message",
-//   },
-// ];
-
 function ConversationList({ handleClick }) {
   const [allUserMessages, setAllUserMessages] = useState([]);
 
@@ -88,17 +40,6 @@ function ConversationList({ handleClick }) {
       );
     });
   });
-
-  // const conversations = users.map((convo, index) => {
-  //   return (
-  //     <div className="conversation" key={`${convo.created}_${index}`}>
-  //       <AccountCircleIcon fontSize="large" />
-  //       <div className="title-text">{convo.name}</div>
-  //       <div className="created-date">{convo.created}</div>
-  //       <div className="conversation-message">{convo.message}</div>
-  //     </div>
-  //   );
-  // });
 
   return <div id="conversation-list">{conversations}</div>;
 }
