3ab073de5a9054454ed294d3f0d14de4225d73fd
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-26 19:47:57 -0400

    Alter messages table

--- database/database.sql
+++ database/database.sql
@@ -8,18 +8,9 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE users RENAME COLUMN first_name TO firstName;
-
-ALTER TABLE users RENAME COLUMN last_name TO lastName;
-
-INSERT INTO users (firstName, lastName, email, PASSWORD)
-    VALUES ('John', 'Smith', 'john@gmail.com', 'password');
-
 CREATE TABLE messages (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
     user_id text NOT NULL,
     message varchar(255) NOT NULL,
     created_at timestamp DEFAULT NOW()
 );
-
-INSERT INTO messages (user_id, message) VALUES ('1', 'Hello, world!');
