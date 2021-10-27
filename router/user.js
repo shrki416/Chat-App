f991de1ca5f3d7063ad033322ec730612600b87f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-27 18:31:36 -0400

    Query for active users

--- router/user.js
+++ router/user.js
@@ -15,4 +15,33 @@ router.get("/user", auth, async (req, res) => {
   }
 });
 
+router.get("/user/active", async (req, res) => {
+  const userQuery = `SELECT id, firstname, lastname FROM users WHERE last_active_at >= now() - interval '1 hr'`;
+  try {
+    const active = await pool.query(userQuery);
+
+    if (active) res.send(active.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Server Error");
+  }
+});
+
 module.exports = router;
+
+// example query to get user's last message
+
+// const messageQuery = `
+//   SELECT id, user_id, message, created_at
+//   FROM messages
+//   WHERE user_id = $1
+//   ORDER BY created_at DESC LIMIT 1
+// `
+
+// ideal data structure I want for conversation list (sidebar):
+
+// {
+//   name: "John Anderson",
+//   created: "1 week ago",
+//   message: "Yes I love how Python does that",
+// },
