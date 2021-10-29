c2ff2b86ef2f08af6855c811aa63193eee6c83b2
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-29 17:06:48 -0400

    Tested db psql function and works as expected

--- database/create-db.sql
+++ database/create-db.sql
@@ -1,10 +1,11 @@
 DO
 $do$
 BEGIN
-    IF EXISTS (SELECT FROM pg_database WHERE datname = 'chat-app') THEN
+    IF EXISTS (SELECT FROM pg_database WHERE datname = "chat-app") THEN
         RAISE NOTICE '❌ Database already exists ❌'; 
     ELSE
-        CREATE DATABASE "chat-app";
+        CREATE EXTENSION IF NOT EXISTS dblink;
+        PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE "chat-app"');
         RAISE NOTICE '✅ Database created ✅';
     END IF;
 END
--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,7 @@
 DO
 $do$
 BEGIN
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
     CREATE TABLE users (
         id uuid DEFAULT uuid_generate_v4() NOT NULL,
         firstname character varying(255) NOT NULL,
@@ -9,13 +10,16 @@ BEGIN
         password character varying(255) NOT NULL,
         last_active_at timestamp with time zone DEFAULT now()
     );
+    RAISE NOTICE `users table created 🎉`;
 
     CREATE TABLE messages (
         id uuid DEFAULT uuid_generate_v4() NOT NULL,
         user_id text NOT NULL,
         message character varying(255) NOT NULL,
         created_at timestamp with time zone DEFAULT now(),
-        receiver_id text NOT NULL,
+        receiver_id text NOT NULL
     );
+    RAISE NOTICE `messages table created 🎉`;
+
 END
 $do$;
