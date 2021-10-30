bcfeaee20a62d37b4aadbf080841d60cd212ec97
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-29 21:42:17 -0400

    move logic to controller

--- router/login.js
+++ router/login.js
@@ -1,51 +1,8 @@
 const router = require("express").Router();
-const pool = require("../database/db");
 const validate = require("../middleware/validate");
-const jwtGenerator = require("../utils/jwtGenerator");
-const bcrypt = require("bcrypt");
+const { login } = require("../controllers/authController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
-router.post("/login", validate, async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-
-    if (user.rows.length === 0)
-      return res
-        .status(401)
-        .send({ message: "Email or Password is inncorrect" });
-
-    const userPassword = user.rows[0].password;
-    const validPassword = await bcrypt.compare(password, userPassword);
-
-    if (!validPassword)
-      return res
-        .status(401)
-        .send({ message: "Email or Password is inncorrect" });
-
-    await pool.query("UPDATE users SET last_active_at = $1 WHERE email = $2", [
-      new Date(),
-      email,
-    ]);
-
-    const activeUsers = await pool.query(
-      `SELECT id, firstname, lastname FROM users WHERE last_active_at >= NOW() - interval '1 hr'`
-    );
-
-    const io = req.app.get("socketio");
-    io.on("connect", (socket) => {
-      socket.emit("login", { activeUsers: activeUsers.rows });
-    });
-
-    const token = jwtGenerator(user.rows[0].id);
-
-    res.json({ token, email });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
-  }
-});
+router.post("/login", validate, catchErrors(login));
 
 module.exports = router;
