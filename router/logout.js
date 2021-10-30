40d24652495c92dd2967e16240b9887580d70043
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-29 21:42:04 -0400

    move logic to controller

--- router/logout.js
+++ router/logout.js
@@ -1,28 +1,7 @@
 const router = require("express").Router();
-const pool = require("../database/db");
+const { logout } = require("../controllers/authController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
-router.post("/logout", async (req, res) => {
-  const { email } = req.body;
-  const io = req.app.get("socketio");
-
-  try {
-    console.log("ran!");
-    await pool.query("UPDATE users SET last_active_at = $1 WHERE email = $2", [
-      null,
-      email,
-    ]);
-
-    const active = await pool.query(
-      `SELECT id, firstname, lastname FROM users WHERE last_active_at > NOW() - interval '1 hr'`
-    );
-
-    io.emit("logout", { activeUsers: active.rows });
-
-    res.json({ message: "Logged Out!" });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
-  }
-});
+router.post("/logout", catchErrors(logout));
 
 module.exports = router;
