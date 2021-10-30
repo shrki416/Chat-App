9ec11c78f22684ce2a52d50c638074e9bf16151f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-30 17:27:25 -0400

    Add user routes into this controller

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+const pool = require("../database/db");
+
+const user = async (req, res) => {
+  try {
+    const user = await pool.query(
+      "SELECT id, firstName, lastName, email, id from users WHERE id=$1",
+      [req.user]
+    );
+    res.json(user.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Server Error");
+  }
+};
+
+const activeUsers = async (req, res) => {
+  const userQuery = `SELECT id, firstname, lastname FROM users WHERE last_active_at >= now() - interval '1 hr'`;
+  try {
+    const active = await pool.query(userQuery);
+
+    if (active) res.send(active.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Server Error");
+  }
+};
+
+module.exports = {
+  user,
+  activeUsers,
+};
