e70e0944da8565f8f3f12288959ce72900a938f1
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-29 21:42:36 -0400

    move logic to controller

--- router/register.js
+++ router/register.js
@@ -1,36 +1,8 @@
 const router = require("express").Router();
-const pool = require("../database/db");
 const validate = require("../middleware/validate");
-const jwtGenerator = require("../utils/jwtGenerator");
-const bcrypt = require("bcrypt");
+const { catchErrors } = require("../handlers/errorHandlers");
+const { register } = require("../controllers/authController");
 
-router.post("/register", validate, async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-
-    const user = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-
-    if (user.rows.length !== 0)
-      return res.status(401).send("User Already Exists");
-
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    const bcryptPassword = await bcrypt.hash(password, salt);
-
-    let newUser = await pool.query(
-      "INSERT INTO users (firstName, lastName, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
-      [firstName, lastName, email, bcryptPassword]
-    );
-
-    const token = jwtGenerator(newUser.rows[0].id);
-
-    res.json({ token });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
-  }
-});
+router.post("/register", validate, catchErrors(register));
 
 module.exports = router;
