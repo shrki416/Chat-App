85d922b150ba16e76d69a9b442e5286646b2b7ee
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-11-08 19:48:42 -0500

    WIP 🚧 working on error handling

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -1,5 +1,5 @@
 const catchErrors = (fn) => {
-  return (req, res, next) => {
+  return function (req, res, next) {
     fn(req, res, next).catch((error) => {
       if (typeof error === "string") {
         res.status(400).json({
@@ -12,8 +12,15 @@ const catchErrors = (fn) => {
   };
 };
 
+const notFound = (req, res, next) => {
+  res.status(404).json({
+    message: "Not Found",
+  });
+};
+
 const devErrors = (error, req, res, next) => {
   error.stack = error.stack || "";
+
   const errorDetails = {
     message: error.message,
     status: error.status,
@@ -21,9 +28,20 @@ const devErrors = (error, req, res, next) => {
   };
 
   res.status(error.status || 500).json(errorDetails);
+  console.log(errorDetails);
+};
+
+const prodErrors = (error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message,
+    error: {},
+  });
 };
 
 module.exports = {
   catchErrors,
   devErrors,
+  prodErrors,
+  notFound,
 };
