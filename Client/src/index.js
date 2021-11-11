c43d7d9a3e2e4fc02a5dede0edfa48e40bab0c1d
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-11-11 15:34:16 -0500

    Add toastify css

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>,
   document.getElementById("root")
 );
