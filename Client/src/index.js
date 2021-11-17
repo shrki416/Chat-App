b7f5b128c31eed610c6bfa4a3f38d8e3af4dadb1
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-11-17 11:16:38 -0500

    Add to styles directory

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./index.css";
+import "./styles/index.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
