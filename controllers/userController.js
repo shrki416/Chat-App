78443c1132548a8a46f78fd8ef0ccd69e4af5898
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-11-23 15:51:35 -0500

    Refactor and clean up

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,23 @@
-const pool = require('../database/db');
-const { getUser } = require('../database/queries/db-user');
+const { getUser, getActiveUsers } = require('../database/queries/db-user');
 
 const users = async (req, res) => {
   try {
     const id = req.user;
-    // const user = await pool.query(
-    //   'SELECT id, firstName, lastName, email, id from users WHERE id=$1',
-    //   [id]
-    // );
     const user = await getUser(id);
     res.json(user);
   } catch (error) {
     console.error(error.message);
-    res.status(500).json('Server Error');
+    res.status(500).json(`Server Error: ${error.message}`);
   }
 };
 
 const activeUsers = async (req, res) => {
-  const userQuery = `SELECT id, firstname, lastname FROM users WHERE last_active_at >= now() - interval '1 hr'`;
   try {
-    const active = await pool.query(userQuery);
-
-    if (active) res.send(active.rows);
+    const active = await getActiveUsers();
+    if (active) res.send(active);
   } catch (error) {
     console.error(error.message);
-    res.status(500).json('Server Error');
+    res.status(500).json(`Server Error: ${error.message}`);
   }
 };
 
