41c37f41bd40a5df60f2c88440694316cbf214b2
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-11-23 15:51:54 -0500

    Refactor and clean up

--- router/auth.js
+++ router/auth.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const validate = require('../middleware/validate');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-const { register } = require('../controllers/authController');
-const { login } = require('../controllers/authController');
-const { logout } = require('../controllers/authController');
+const { register, login, logout } = require('../controllers/authController');
 
 router.post('/register', validate, catchErrors(register));
 router.post('/login', validate, catchErrors(login));
