7a4241c75367bdd162a4c4b9e3eaf531852d936a
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-11-29 17:38:16 -0500

    WIP

--- Client/src/components/ConversationList.js
+++ Client/src/components/ConversationList.js
@@ -1,113 +1,55 @@
 import "../styles/Chat.css";
 
+import * as timeago from "timeago.js";
+
 import React, { useEffect, useState } from "react";
 
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import axios from "axios";
 
-// import * as timeago from "timeago.js";
-
-// const users = [
-//   {
-//     name: "John Anderson",
-//     created: "1 week ago",
-//     message: "Yes I love how Python does that",
-//   },
-// ];
-
 function ConversationList({ handleClick }) {
-    const [users, setUsers] = useState([]);
-//   const [allUserMessages, setAllUserMessages] = useState([]);
+  const [allUserMessages, setAllUserMessages] = useState([]);
+
 
   useEffect(() => {
-    // getAllUsersMessages();
-    test();
-    getUsers();
+    getAllUsersMessages();
   }, []);
 
-  async function getUsers() {
+  async function getAllUsersMessages() {
     try {
-        const response = await axios.get(`/api/users`)
-        console.log(`🍎`, response.data);
-        // setUsers(response.data);
+      const response = await axios.get(`/api/userMessages`);
+      setAllUserMessages(response.data);
     } catch (error) {
-        console.log(error.message);
+      console.error(error.message);
     }
   }
 
-  function test() {
-    const id = localStorage.getItem("me");
-    console.log(id);
-    axios
-      .get(`/api/userMessages/${id}`)
-      .then((res) => {
-          console.log(`🍆`, res);
-        //   setAllUserMessages(res.data)
-        })
-      .catch((err) => console.log(err));
-  }
+  const conversations = allUserMessages.map((userMessage) => {
+    const { id, name, email, message } = userMessage;
 
-
-//   function getAllUsersMessages() {
-//     axios
-//       .get(`/api/userMessages`)
-//       .then((res) => setAllUserMessages(res.data))
-//       .catch((err) => console.log(err));
-//   }
-
-//   const conversations = allUserMessages.map((userMessage) => {
-//     const { id, firstname, lastname, email, messages } = userMessage;
-//     const fullName = `${firstname} ${lastname}`;
-
-//     messages.forEach((message) => {
-//       if (message !== null) {
-//         message.forEach((msg) => {
-//           msg["userEmail"] = email;
-//           msg["from"] = fullName;
-//         });
-//       }
-//     });
-
-//     return messages.map((message) => {
-//       let lastMessage = "";
-//       if (message !== null) {
-//         lastMessage = message[message.length - 1];
-//       }
-//       const { created_at, message: lastMessageText } = lastMessage;
-//       const created = timeago.format(created_at, "en_US");
-
-//       return (
-//         <div className="conversation" key={userMessage.id}>
-//           <AccountCircleIcon fontSize="large" />
-//           <div
-//             className="title-text"
-//             onClick={(e) => handleClick(e, id, messages)}
-//           >
-//             {fullName}
-//           </div>
-//           <div className="created-date">{created}</div>
-//           <div className="conversation-message">{lastMessageText}</div>
-//         </div>
-//       );
-//     });
-//   });
-
-//   return <div id="conversation-list">{conversations}</div>;
-    return (
-        <div id="conversation-list">
-            <div className="conversation">
-            <AccountCircleIcon fontSize="large" />
-            <div
-                className="title-text"
-                // onClick={(e) => handleClick(e, id, messages)}
-            >
-                {users.map(({firstname, lastname}) => `${firstname} ${lastname}`)}
-            </div>
-            <div className="created-date">test date</div>
-            <div className="conversation-message">test message</div>
-            </div>
-        </div>
+    const lastMessage = message.map((msg => msg.message));
+    const created = message.map((msg) =>
+      timeago.format(msg.created_at, "en_US")
     );
+
+    // message.forEach((msg) => {
+    //   msg["userEmail"] = email;
+    //   msg["from"] = name;
+    // });
+
+    return (
+      <div className="conversation" key={id}>
+        <AccountCircleIcon fontSize="large" />
+        <div className="title-text" onClick={(e) => handleClick(e, id)}>
+          {name}
+        </div>
+        <div className="created-date">{created}</div>
+        <div className="conversation-message">{lastMessage}</div>
+      </div>
+    );
+  });
+
+  return <div id="conversation-list">{conversations}</div>;
 }
 
 export default ConversationList;
