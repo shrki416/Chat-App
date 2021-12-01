6a4b56d728ec1fe360fbc9f9b910ff606ed7254c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-01 16:53:54 -0500

    WIP: conversation list is not updating in realtime

--- Client/src/components/ConversationList.js
+++ Client/src/components/ConversationList.js
@@ -10,31 +10,28 @@ import axios from "axios";
 function ConversationList({ handleClick }) {
   const [messageMetaData, setMessageMetaData] = useState([]);
   const LOGGED_IN_USER = localStorage.getItem("me");
-  const [active, setActive] = useState(true);
-
 
   useEffect(() => {
+    async function getUserLastMessage() {
+        try {
+        const response = await axios.get(`/api/userMessages`);
+        setMessageMetaData(response.data);
+        } catch (error) {
+        console.error(error.message);
+        }
+    }
+
     getUserLastMessage();
-
-
     return () => {
-        // Clean up
-    }
-  });
+      // Clean up
+    };
+  }, []);
 
-  async function getUserLastMessage() {
-    try {
-      const response = await axios.get(`/api/userMessages`);
-      setMessageMetaData(response.data);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
 
   const conversations = messageMetaData.map((userMessage) => {
-    const { id, name, message } = userMessage;
+    const { id, name, active, message } = userMessage;
 
-    const lastMessage = message.map((msg => msg.message));
+    const lastMessage = message.map((msg) => msg.message);
     const created = message.map((msg) =>
       timeago.format(msg.created_at, "en_US")
     );
@@ -47,14 +44,13 @@ function ConversationList({ handleClick }) {
       <div className="conversation" key={id}>
         <AccountCircleIcon fontSize="large" />
         <div className="title-text" onClick={(e) => handleClick(e, id)}>
-          {name} <span className={`${active ? 'online' : 'offline'}`}></span>
+          {name} <span className={`${active ? "online" : "offline"}`}></span>
         </div>
         <div className="created-date">{created}</div>
         <div className="conversation-message">{lastMessage}</div>
       </div>
     );
   });
-
 
   return <div id="conversation-list">{conversations}</div>;
 }
