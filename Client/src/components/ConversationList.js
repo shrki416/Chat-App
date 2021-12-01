add6a88476fdc059248f260e8c273bc88eb7283f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-11-30 20:15:09 -0500

    WIP: displaying active users

--- Client/src/components/ConversationList.js
+++ Client/src/components/ConversationList.js
@@ -8,51 +8,53 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import axios from "axios";
 
 function ConversationList({ handleClick }) {
-  const [allUserMessages, setAllUserMessages] = useState([]);
+  const [messageMetaData, setMessageMetaData] = useState([]);
+  const LOGGED_IN_USER = localStorage.getItem("me");
+  const [active, setActive] = useState(true);
 
 
   useEffect(() => {
-    getAllUsersMessages();
+    getUserLastMessage();
 
-    // TODO: Properly clean up the useEffect
+
     return () => {
-    //   getAllUsersMessages();
+        // Clean up
     }
   });
 
-  async function getAllUsersMessages() {
+  async function getUserLastMessage() {
     try {
       const response = await axios.get(`/api/userMessages`);
-      setAllUserMessages(response.data);
+      setMessageMetaData(response.data);
     } catch (error) {
       console.error(error.message);
     }
   }
 
-  const conversations = allUserMessages.map((userMessage) => {
-    const { id, name, email, message } = userMessage;
+  const conversations = messageMetaData.map((userMessage) => {
+    const { id, name, message } = userMessage;
 
     const lastMessage = message.map((msg => msg.message));
     const created = message.map((msg) =>
       timeago.format(msg.created_at, "en_US")
     );
 
-    // message.forEach((msg) => {
-    //   msg["userEmail"] = email;
-    //   msg["from"] = name;
-    // });
+    if (LOGGED_IN_USER === id) {
+      return null;
+    }
 
     return (
       <div className="conversation" key={id}>
         <AccountCircleIcon fontSize="large" />
         <div className="title-text" onClick={(e) => handleClick(e, id)}>
-          {name}
+          {name} <span className={`${active ? 'online' : 'offline'}`}></span>
         </div>
         <div className="created-date">{created}</div>
         <div className="conversation-message">{lastMessage}</div>
       </div>
     );
   });
+
 
   return <div id="conversation-list">{conversations}</div>;
 }
