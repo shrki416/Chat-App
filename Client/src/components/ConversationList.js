a5cfe7c22ee1a64f257f786acc548c594c6e867e
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-04 14:39:31 -0500

    WIP: update last message

--- Client/src/components/ConversationList.js
+++ Client/src/components/ConversationList.js
@@ -25,37 +25,6 @@ function ConversationList({ handleClick, lastReceivedMessage }) {
     getUserLastMessage();
   }, [checkForNewMessages]);
 
-  //   console.log({ messageMetaData });
-
-  //   useEffect(() => {
-  //     if (lastReceivedMessage.receiver_id) {
-  //       const me = localStorage.getItem("me");
-
-  //       let modiList = [...messageMetaData];
-
-  //       console.log({ lastReceivedMessage });
-  //       modiList.length &&
-  //         modiList.forEach((item, index) => {
-  //           console.log(item);
-
-  //           if (
-  //             (item.id === lastReceivedMessage.receiver_id &&
-  //               me === lastReceivedMessage.user_id) ||
-  //             (item.id === lastReceivedMessage.user_id &&
-  //               me === lastReceivedMessage.receiver_id)
-  //           ) {
-  //             modiList[index].message = [
-  //               {
-  //                 ...lastReceivedMessage,
-  //                 receiverId: lastReceivedMessage.receiver_id,
-  //               },
-  //             ];
-  //           }
-  //         });
-  //       setMessageMetaData(modiList);
-  //     }
-  //   }, [JSON.stringify(lastReceivedMessage)]);
-
   const conversations = messageMetaData.map((userMessage) => {
     const { id, name, active, message } = userMessage;
 
@@ -63,14 +32,12 @@ function ConversationList({ handleClick, lastReceivedMessage }) {
     const created = message.map((msg) =>
       timeago.format(msg.created_at, "en_US")
     );
+    const sender = message.map((msg) => msg.receiverId);
+    console.log(sender);
 
     if (LOGGED_IN_USER === id) {
       return null;
     }
-
-    // const receivedByMe = message[0]?.receiverId === id;
-
-    // console.log({ userMessage, receivedByMe, message: message[0], id });
 
     return (
       <div className="conversation" key={id}>
@@ -79,10 +46,7 @@ function ConversationList({ handleClick, lastReceivedMessage }) {
           {name} <span className={`${active ? "online" : "offline"}`}></span>
         </div>
         <div className="created-date">{created}</div>
-        <div className="conversation-message">
-          {/* {receivedByMe ? "I Sent:" : "Received:"} {lastMessage} */}
-          {lastMessage}
-        </div>
+        <div className="conversation-message">{lastMessage}</div>
       </div>
     );
   });
