3c607300cc78fdd237943bcd864dbdee5fc91dba
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-04 14:38:02 -0500

    Refactor auth queries

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,17 @@
 const bcrypt = require('bcrypt');
-const pool = require('../database/db');
 const jwtGenerator = require('../utils/jwtGenerator');
+const {
+  userQuery,
+  registerNewUserQuery,
+  updateActiveUsersOnLoginQuery,
+  queryActiveUsers,
+  updateActiveUsersOnLogoutQuery,
+} = require('../database/queries/db-auth.js');
 
 const register = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  const user = await pool.query('SELECT * FROM users WHERE email = $1', [
-    email,
-  ]);
+  const user = await userQuery(email);
 
   if (user.rows.length !== 0)
     return res.status(401).send('User Already Exists');
@@ -16,50 +20,44 @@ const register = async (req, res) => {
   const salt = await bcrypt.genSalt(saltRounds);
   const bcryptPassword = await bcrypt.hash(password, salt);
 
-  const newUser = await pool.query(
-    'INSERT INTO users (firstName, lastName, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
-    [firstName, lastName, email, bcryptPassword]
+  const newUser = await registerNewUserQuery(
+    firstName,
+    lastName,
+    email,
+    bcryptPassword
   );
 
-  const token = jwtGenerator(newUser.rows[0].id);
+  const token = jwtGenerator(newUser);
   const message = 'User Created Successfully';
-  console.log(message);
 
   res.json({ token, message });
 };
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const user = await pool.query('SELECT * FROM users WHERE email = $1', [
-    email,
-  ]);
+  const user = await userQuery(email);
 
-  if (user.rows.length === 0)
+  if (!user)
     return res.status(401).send({ message: 'Email or Password is inncorrect' });
 
-  const userPassword = user.rows[0].password;
+  const userPassword = user.password;
   const validPassword = await bcrypt.compare(password, userPassword);
 
   if (!validPassword)
     return res.status(401).send({ message: 'Email or Password is inncorrect' });
 
-  await pool.query('UPDATE users SET last_active_at = $1 WHERE email = $2', [
-    new Date(),
-    email,
-  ]);
+  updateActiveUsersOnLoginQuery(email);
 
-  const activeUsers = await pool.query(
-    `SELECT id, firstname, lastname FROM users WHERE last_active_at >= NOW() - interval '1 hr'`
-  );
+  const activeUsers = await queryActiveUsers();
 
   const io = req.app.get('socketio');
   io.on('connect', (socket) => {
-    socket.emit('login', { activeUsers: activeUsers.rows });
+    socket.emit('login', { activeUsers });
   });
 
-  const token = jwtGenerator(user.rows[0].id);
-  const firstName = user.rows[0].firstname;
+  const token = jwtGenerator(user.id);
+  const firstName = user.firstname;
 
   res.json({ token, email, firstName });
 };
@@ -68,26 +66,13 @@ const logout = async (req, res) => {
   const { email } = req.body;
   const io = req.app.get('socketio');
 
-  await pool.query('UPDATE users SET last_active_at = $1 WHERE email = $2', [
-    null,
-    email,
-  ]);
+  updateActiveUsersOnLogoutQuery(email);
 
-  const active = await pool.query(
-    `SELECT id, firstname, lastname FROM users WHERE last_active_at > NOW() - interval '1 hr'`
-  );
+  const activeUsers = await queryActiveUsers();
 
-  io.emit('logout', { activeUsers: active.rows });
+  io.emit('logout', { activeUsers });
 
-  const userFirstname = await pool.query(
-    `SELECT firstName FROM users WHERE email = $1`,
-    [email]
-  );
-
-  const name = userFirstname.rows[0].firstname;
-
-  io.emit('logout', { name });
-  res.json({ message: `${name} is logged out!` });
+  res.json({ message: `Bye 👋 see you soon!` });
 };
 
 module.exports = {
