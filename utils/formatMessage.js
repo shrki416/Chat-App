292fca4744274edce73d23522206c4993cbea805
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-07 10:46:45 -0500

    Refactor and clean up

--- utils/formatMessage.js
+++ utils/formatMessage.js
@@ -1,12 +1,11 @@
 function formatMessage(query) {
   return query.map((messageData) => {
     // eslint-disable-next-line camelcase
-    const { id, firstname, lastname, last_active_at, messages } = messageData;
+    const { id, firstname, lastname, messages } = messageData;
 
     const data = {
       id,
       name: `${firstname} ${lastname}`,
-      active: last_active_at,
       message: messages.map((message) => message[message.length - 1]) || [],
     };
 
