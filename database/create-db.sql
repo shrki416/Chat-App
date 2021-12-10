0f50e59add9fb4d33ebbd9e18f517c93e07a9b01
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-10 12:10:13 -0500

    Change db creation script

--- database/create-db.sql
+++ database/create-db.sql
@@ -1,11 +1,11 @@
 DO
 $do$
 BEGIN
-    IF EXISTS (SELECT FROM pg_database WHERE datname = "chat-app") THEN
+    IF EXISTS (SELECT FROM pg_database WHERE datname = 'chat') THEN
         RAISE NOTICE '❌ Database already exists ❌';
     ELSE
         CREATE EXTENSION IF NOT EXISTS dblink;
-        PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE "chat-app"');
+        PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE "chat"');
         RAISE NOTICE '✅ Database created ✅';
     END IF;
 END
