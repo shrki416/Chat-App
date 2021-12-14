f707b7a9262946496818ed8498abcb147c07b42c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-14 17:12:13 -0500

    Add emoji icon

--- Client/src/components/ChatForm.js
+++ Client/src/components/ChatForm.js
@@ -1,6 +1,7 @@
 import "../styles/ChatForm.css";
 
 import IconButton from "@mui/material/IconButton";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import React from "react";
 import SendIcon from "@mui/icons-material/Send";
 
@@ -13,6 +14,9 @@ function ChatForm({ handleSubmit, setInput, input }) {
         placeholder="type a message..."
         onChange={(e) => setInput(e.target.value)}
       />
+      <IconButton>
+        <InsertEmoticonIcon id="emoji-icon" />
+      </IconButton>
       <IconButton onClick={handleSubmit}>
         <SendIcon alt="send message" id="send-icon" />
       </IconButton>
