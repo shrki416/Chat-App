dc7936bc8db9ab869bd6618c810c635ed9c73cf1
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-14 17:14:37 -0500

    WIP: chat rooms

--- router/message.js
+++ router/message.js
@@ -3,11 +3,15 @@ const {
   privateMessage,
   messages,
   userMessages,
+  createChannelMessages,
+  getChannelMessages,
 } = require('../controllers/messageController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/message/:userId/:chatMateId', catchErrors(privateMessage));
 router.post('/message', catchErrors(messages));
 router.get('/userMessages', catchErrors(userMessages));
+router.post('/channel', catchErrors(createChannelMessages));
+router.get('/channel/:channel', catchErrors(getChannelMessages));
 
 module.exports = router;
