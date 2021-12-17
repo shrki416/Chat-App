5e474b45180d6173221e1ff5c6407c62bd7fbc3c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-17 17:15:51 -0500

    chat rooms done

--- router/user.js
+++ router/user.js
@@ -4,6 +4,6 @@ const { catchErrors } = require('../handlers/errorHandlers');
 const { users, activeUsers } = require('../controllers/userController');
 
 router.get('/user', auth, catchErrors(users));
-router.get('/user/active', catchErrors(activeUsers));
+router.get('/user/active', auth, catchErrors(activeUsers));
 
 module.exports = router;
