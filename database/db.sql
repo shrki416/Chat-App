10deda971f2b8d4bfba188cc0e2698a1cce7ae9b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-18 11:00:26 -0500

    Update db.sql file

--- database/db.sql
+++ database/db.sql
@@ -1,24 +1,36 @@
-DO
-$do$
+DO $do$
 BEGIN
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
     CREATE TABLE users (
-        id uuid DEFAULT uuid_generate_v4() NOT NULL,
-        firstname character varying(255) NOT NULL,
-        lastname character varying(255) NOT NULL,
-        email character varying(255) NOT NULL,
-        password character varying(255) NOT NULL,
-        last_active_at timestamp with time zone DEFAULT now()
+        id uuid DEFAULT uuid_generate_v4 ( ) NOT NULL,
+        firstname character varying(255 ) NOT NULL,
+        lastname character varying(255 ) NOT NULL,
+        email character varying(255 ) NOT NULL,
+        PASSWORD character varying(255 ) NOT NULL,
+        last_active_at timestamp with time zone DEFAULT now( )
     );
     RAISE NOTICE 'users table created 🎉';
-
     CREATE TABLE messages (
-        id uuid DEFAULT uuid_generate_v4() NOT NULL,
+        id uuid DEFAULT uuid_generate_v4 ( ) NOT NULL,
         user_id text NOT NULL,
-        message character varying(255) NOT NULL,
-        created_at timestamp with time zone DEFAULT now(),
+        message character varying(255 ) NOT NULL,
+        created_at timestamp with time zone DEFAULT now( ),
         receiver_id text NOT NULL
     );
     RAISE NOTICE 'messages table created 🎉';
+    CREATE TABLE rooms (
+        id uuid DEFAULT uuid_generate_v4 ( ) NOT NULL,
+        room_name text NOT NULL
+    );
+    RAISE NOTICE 'rooms table created 🎉';
+    CREATE TABLE room_messages (
+        id uuid DEFAULT uuid_generate_v4 ( ) NOT NULL,
+        room_id text NOT NULL,
+        message text NOT NULL,
+        user_id text NOT NULL,
+        created_at timestamp with time zone DEFAULT now( )
+    );
+    RAISE NOTICE 'room_messages table created 🎉';
 END
 $do$;
+
