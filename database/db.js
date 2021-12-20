c03e73ddd38eb90fa9f427dc732a29c6330f6a43
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-20 17:13:35 -0500

    Refactor and clean up

--- database/db.js
+++ database/db.js
@@ -2,11 +2,15 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.HOST,
   user: process.env.USER,
-  database: process.env.DATABASE,
   password: process.env.PASSWORD,
-  port: 5432,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  //   connectionString: process.env.DATABASE_URL,
+  //   port: 5432,
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
 });
 
 pool
