9ecb21a10d2501dbc3e3f4c82bf48a7ae04464c4
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-20 17:12:48 -0500

    Refactor and clean up

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const socket = require('socket.io');
-const http = require('http');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 const { devErrors, prodErrors } = require('./handlers/errorHandlers');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const server = http.createServer(app);
-const io = socket(server);
+const server = createServer(app);
+const io = new Server(server);
 app.set('socketio', io);
 
 app.use(express.static(path.join(__dirname, 'Client/build')));
@@ -20,15 +20,20 @@ app.use('/api', require('./router/verify'));
 app.use('/api', require('./router/user'));
 app.use('/api', require('./router/message'));
 
-io.sockets.on('connection', (soc) => {
-  soc.on('join', ({ userId }) => {
-    soc.join(userId);
-  });
+// io.sockets.on('connection', (soc) => {
+//   soc.on('join', ({ userId }) => {
+//     console.log(userId);
+//     // soc.join(userId);
+//   });
 
-  soc.on('room', ({ roomId }) => {
-    soc.join(roomId);
-  });
-});
+//   soc.on('room-chat', ({ roomId }) => {
+//     soc.join(roomId);
+//   });
+// });
+
+// io.on('connection', (socket) => {
+//   console.log(`🙂`, socket.id);
+// });
 
 if (process.env.ENV === 'DEVELOPMENT') {
   app.use(devErrors);
