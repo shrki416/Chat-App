319e96e139199df001735d1a63e32d7628062cab
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-21 10:21:53 -0500

    Refactor room queries

--- router/message.js
+++ router/message.js
@@ -4,20 +4,12 @@ const {
   privateMessage,
   messages,
   userMessages,
-  createChannelMessages,
-  getChannelMessages,
-  getChannel,
-  getAllChannels,
 } = require('../controllers/messageController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.post('/message', auth, catchErrors(messages));
-router.post('/channel', auth, catchErrors(createChannelMessages));
-router.get('/channels', auth, catchErrors(getAllChannels));
 
 router.get('/message/:userId/:chatMateId', auth, catchErrors(privateMessage));
 router.get('/userMessages', auth, catchErrors(userMessages));
-router.get('/channel/:channel', auth, catchErrors(getChannelMessages));
-router.get('/channel/id/:channel', auth, catchErrors(getChannel));
 
 module.exports = router;
