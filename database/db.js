13fe67e878301ef946f00c210092d841bfe67a94
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-21 10:20:32 -0500

    Add different configuration whether in dev vs prod

--- database/db.js
+++ database/db.js
@@ -1,21 +1,33 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+/* Development Database Connection */
+
+// const pool = new Pool({
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   port: process.env.PORT,
+// });
+
+/* Production Database Connection */
+
 const pool = new Pool({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  //   connectionString: process.env.DATABASE_URL,
-  //   port: 5432,
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
+  connectionString: process.env.DATABASE_URL,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool
-  .connect()
-  .then(() => console.log('👌 connected to database 👌'))
-  .catch((err) => console.log(err));
+pool.connect((err, client, release) => {
+  if (err) return console.error('Error acquiring client', err.stack);
+  client.query('SELECT NOW()', (err, result) => {
+    release();
+    if (err) return console.error('Error executing query', err.stack);
+    console.log(`👌 ${result.rows[0].now}: connected to db 👌`);
+  });
+});
 
 module.exports = pool;
