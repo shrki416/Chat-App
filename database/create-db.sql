1262ab00d255838719401b6f87b7ab341dbd6edc
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-21 10:23:13 -0500

    Update db creation file

--- database/create-db.sql
+++ database/create-db.sql
@@ -1,12 +1,18 @@
-DO
-$do$
+DO $do$
 BEGIN
-    IF EXISTS (SELECT FROM pg_database WHERE datname = 'chat') THEN
-        RAISE NOTICE '❌ Database already exists ❌';
-    ELSE
-        CREATE EXTENSION IF NOT EXISTS dblink;
-        PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE "chat"');
-        RAISE NOTICE '✅ Database created ✅';
-    END IF;
+    IF EXISTS (
+        SELECT
+        FROM
+            pg_database
+        WHERE
+            datname = "chat-app") THEN
+    RAISE NOTICE '❌ Database already exists ❌';
+ELSE
+    CREATE EXTENSION IF NOT EXISTS dblink;
+    PERFORM
+        dblink_exec('dbname=' || current_database(), 'CREATE DATABASE "chat-app"');
+    RAISE NOTICE '✅ Database created ✅';
+END IF;
 END
 $do$;
+
