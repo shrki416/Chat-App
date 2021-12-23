387e74e14dacdeae1e0a35f4ff7339e88092aa55
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-12-22 21:16:22 -0500

    Refactor and clean-up

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,10 +3,6 @@ const {
   privateMessagesQuery,
   createMessageQuery,
   userMessagesQuery,
-  //   createChannelMsgQuery,
-  //   channelMessageQuery,
-  //   getChannelId,
-  //   getChannels,
 } = require('../database/queries/db-message');
 
 const privateMessage = async (req, res) => {
@@ -43,60 +39,8 @@ const userMessages = async (req, res) => {
   }
 };
 
-// const createChannelMessages = async (req, res) => {
-//   const io = req.app.get('socketio');
-//   try {
-//     const { userId, message, from, channelId } = req.body;
-
-//     const createMessage = await createChannelMsgQuery(
-//       userId,
-//       message,
-//       channelId
-//     );
-//     const result = { ...createMessage.rows[0], from };
-
-//     io.emit('public-message', result);
-//   } catch (error) {
-//     res.status(500).send(error.message);
-//   }
-// };
-
-// const getChannelMessages = async (req, res) => {
-//   try {
-//     const { channel } = req.params;
-//     const channelMessages = await channelMessageQuery(channel);
-
-//     res.send(channelMessages);
-//   } catch (error) {
-//     res.status(500).send(error.message);
-//   }
-// };
-
-// const getChannel = async (req, res) => {
-//   try {
-//     const { channel } = req.params;
-//     const { id } = await getChannelId(channel);
-//     res.send(id);
-//   } catch (error) {
-//     res.status(500).send(error.message);
-//   }
-// };
-
-// const getAllChannels = async (req, res) => {
-//   try {
-//     const channels = await getChannels();
-//     res.send(channels);
-//   } catch (error) {
-//     res.status(500).send(error.message);
-//   }
-// };
-
 module.exports = {
   privateMessage,
   messages,
   userMessages,
-  //   createChannelMessages,
-  //   getChannelMessages,
-  //   getChannel,
-  //   getAllChannels,
 };
