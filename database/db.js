e37e0ef8483dc55181ba34b6dbb20b4048742b0c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-31 11:55:39 -0500

    testing

--- database/db.js
+++ database/db.js
@@ -1,22 +1,57 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool =
+//   process.env.ENV === 'DEVELOPMENT'
+//     ? new Pool({
+//         user: process.env.USER,
+//         host: process.env.HOST,
+//         database: process.env.DATABASE,
+//         password: process.env.PASSWORD,
+//         port: 5432,
+//       })
+//     : new Pool({
+//         connectionString: process.env.DATABASE_URL,
+//         port: 5432,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       });
+
+// pool.connect((err, client, release) => {
+//   if (err) return console.error('Error acquiring client', err.stack);
+//   client.query('SELECT NOW()', (err, result) => {
+//     release();
+//     if (err) return console.error('Error executing query', err.stack);
+//     console.log(`👌 ${result.rows[0].now}: connected to db 👌`);
+//     client.end();
+//   });
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool =
-  process.env.ENV === 'DEVELOPMENT'
-    ? new Pool({
-        user: process.env.USER,
-        host: process.env.HOST,
-        database: process.env.DATABASE,
-        password: process.env.PASSWORD,
-        port: 5432,
-      })
-    : new Pool({
-        connectionString: process.env.DATABASE_URL,
-        port: 5432,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      });
+/* Development Database Connection */
+
+// const pool = new Pool({
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   port: process.env.PORT,
+// });
+
+/* Production Database Connection */
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 pool.connect((err, client, release) => {
   if (err) return console.error('Error acquiring client', err.stack);
