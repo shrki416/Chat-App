2a0d65f05d26def5dd41b814eb75077a67fc804d
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-02-09 13:52:29 -0500

    WIP: too many connections issue

--- database/create-db.sql
+++ database/create-db.sql
@@ -1,17 +1,17 @@
 DO $do$
 BEGIN
-    IF EXISTS (
-        SELECT
-        FROM
-            pg_database
-        WHERE
-            datname = "chat-app") THEN
-    RAISE NOTICE '❌ Database already exists ❌';
+  IF EXISTS (
+    SELECT
+    FROM
+      pg_database
+    WHERE
+      datname = "chat-app") THEN
+  RAISE NOTICE '❌ Database already exists ❌';
 ELSE
-    CREATE EXTENSION IF NOT EXISTS dblink;
-    PERFORM
-        dblink_exec('dbname=' || current_database(), 'CREATE DATABASE "chat-app"');
-    RAISE NOTICE '✅ Database created ✅';
+  CREATE EXTENSION IF NOT EXISTS dblink;
+  PERFORM
+    dblink_exec('dbname=' || current_database(), 'CREATE DATABASE "chat-app"');
+  RAISE NOTICE '✅ Database created ✅';
 END IF;
 END
 $do$;
--- database/db.js
+++ database/db.js
@@ -1,30 +1,52 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool =
+//   process.env.ENV === 'DEVELOPMENT'
+//     ? new Pool({
+//         user: process.env.USER,
+//         host: process.env.HOST,
+//         database: process.env.DATABASE,
+//         password: process.env.PASSWORD,
+//         port: 5432,
+//       })
+//     : new Pool({
+//         connectionString: process.env.DATABASE_URL,
+//         port: 5432,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       });
+
+// pool.connect((err, client, release) => {
+//   if (err) return console.error('Error acquiring client', err.stack);
+//   client.query('SELECT NOW()', (err, result) => {
+//     release();
+//     if (err) return console.error('Error executing query', err.stack);
+//     console.log(`👌 ${result.rows[0].now}: connected to db 👌`);
+//     client.end();
+//   });
+// });
+
+// module.exports = pool;
+
+const pg = require('pg');
 require('dotenv').config();
 
-const pool =
-  process.env.ENV === 'DEVELOPMENT'
-    ? new Pool({
-        user: process.env.USER,
-        host: process.env.HOST,
-        database: process.env.DATABASE,
-        password: process.env.PASSWORD,
-        port: 5432,
-      })
-    : new Pool({
-        connectionString: process.env.DATABASE_URL,
-        port: 5432,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      });
+const conString = process.env.DATABASE_URL; // Can be found in the Details page
+const pool = new pg.Client(conString);
 
-pool.connect((err, client, release) => {
-  if (err) return console.error('Error acquiring client', err.stack);
-  client.query('SELECT NOW()', (err, result) => {
-    release();
-    if (err) return console.error('Error executing query', err.stack);
-    console.log(`👌 ${result.rows[0].now}: connected to db 👌`);
-    client.end();
+pool.connect((err) => {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+  pool.query('SELECT NOW() AS "theTime"', (err, result) => {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    // pool.end();
   });
 });
 
