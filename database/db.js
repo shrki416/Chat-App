f3880f0402d22a73daef248f373196194aa5b77c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-02-09 14:20:30 -0500

    Patched

--- database/db.js
+++ database/db.js
@@ -1,52 +1,19 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool =
-//   process.env.ENV === 'DEVELOPMENT'
-//     ? new Pool({
-//         user: process.env.USER,
-//         host: process.env.HOST,
-//         database: process.env.DATABASE,
-//         password: process.env.PASSWORD,
-//         port: 5432,
-//       })
-//     : new Pool({
-//         connectionString: process.env.DATABASE_URL,
-//         port: 5432,
-//         ssl: {
-//           rejectUnauthorized: false,
-//         },
-//       });
-
-// pool.connect((err, client, release) => {
-//   if (err) return console.error('Error acquiring client', err.stack);
-//   client.query('SELECT NOW()', (err, result) => {
-//     release();
-//     if (err) return console.error('Error executing query', err.stack);
-//     console.log(`👌 ${result.rows[0].now}: connected to db 👌`);
-//     client.end();
-//   });
-// });
-
-// module.exports = pool;
-
 const pg = require('pg');
 require('dotenv').config();
 
-const conString = process.env.DATABASE_URL; // Can be found in the Details page
+const conString =
+  process.env.ENV === 'DEVELOPMENT'
+    ? process.env.DEV_DATABASE_URL
+    : process.env.PROD_DATABASE_URL;
+
 const pool = new pg.Client(conString);
 
 pool.connect((err) => {
-  if (err) {
-    return console.error('could not connect to postgres', err);
-  }
+  if (err) return console.error('could not connect to postgres', err.stack);
+
   pool.query('SELECT NOW() AS "theTime"', (err, result) => {
-    if (err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    // >> output: 2018-08-23T14:02:57.117Z
-    // pool.end();
+    if (err) return console.error('error running query', err);
+    console.log(`👌 ${result.rows[0].theTime}: connected to db 👌`);
   });
 });
 
